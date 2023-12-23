b5e5c6068b92a8fa6712b19a806cb41cdd9e5cb0
DHANASEKAR.N <dhanasekar.n@collpoll.com>
2023-12-23 20:16:32 +0530

    Added simple project with inefficient code to check the sonar cloud analysis and updated the sonar cloud yml

---
+++ HOLA/HOLA/src/main/java/com/hola/HOLA/HolaController.java
@@ -0,0 +1,38 @@
+package com.hola.HOLA;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HolaController {
+
+    @GetMapping("/hola")
+    public String index() {
+        int result = calculateSum(10, 20);
+        System.out.println("Result: " + result);
+        return String.format("Greetings from Spring Boot! Result is %d", result);
+
+    }
+
+    private static int calculateSum(int a, int b) {
+        // Redundant variables and unnecessary operations
+        int x = a;
+        int y = b;
+        int sum = 0;
+
+        for (int i = 0; i < 10; i++) {
+            sum = sum + x + y;
+            // Unnecessary conditional statement
+            if (i % 2 == 0) {
+                // Unused variable
+                int temp = x;
+                x = y;
+                y = temp;
+            }
+        }
+
+        // Return inside the loop
+        return sum;
+    }
+
+}
---
+++ HOLA/HOLA/src/test/java/com/hola/HOLA/HolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hola.HOLA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
